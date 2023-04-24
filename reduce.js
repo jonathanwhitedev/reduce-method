d6a4a4a10679bdb4cb1360062dfe0f01b76153ea
jonathanwhitedev <white_jonathan@icloud.com>
2023-04-24 16:00:10 +0000

    finished advanced JS

--- reduce.js
+++ reduce.js
@@ -6,7 +6,14 @@
 
 
 // Summing an array of numbers:
-
+const nums = [0, 1 ,2 ,3 ,4];
+let sum = nums.reduce((acc, curr) => acc + curr, 0);
+// "0" start accumulator number (an interger that can be anything you want, you should always specify the initia; value)
+//Accumulator: 0 Current Value: 1 Total: 1
+//Accumulator: 1 Current Value: 2 Total: 3
+//Accumulator: 3 Current Value: 3 Total: 6
+//Accumulator: 6 Current Value: 4 Total: 10
+console.log(sum);
 
 const teamMembers = [
   {
@@ -28,10 +35,28 @@ const teamMembers = [
     name: 'Kelly',
     profession: 'Designer',
     yrsExperience: 3
+  },
+  {
+    name: 'Joanna',
+    profession: 'Manager',
+    yrsExperience: 10
   }
 ];
 
 // Totaling a specific object property
-
+let totalExperience = teamMembers.reduce((acc, curr) => acc + curr.yrsExperience, 0); //always specify the number where you want to start!!
+console.log(totalExperience);
 
 // Grouping by a property, and totaling it too
+// {Developer: 12, Designer: 4} <--- this is what we want!
+let experienceByProfession = teamMembers.reduce((acc, curr) => {
+  let key = curr.profession;
+  if (!acc[key]) {
+    acc[key] = curr.yrsExperience;
+  } else {
+    acc[key] += curr.yrsExperience;
+  }
+  return acc;
+}, {});
+
+console.log(experienceByProfession);
